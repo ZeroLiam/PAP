0d2df07c205bb51602239bfa1ac9ab6cc0cc3c68
ZeroLiam <liselotr@gmail.com>
2017-06-27 11:34:10 +0200

    Added config.protocol to ws_server.js

--- PhoneAsPixel/app/lib/ws_server.js
+++ PhoneAsPixel/app/lib/ws_server.js
@@ -35,7 +35,7 @@ wsServer.on('request', function(request) {
       return;
     }
 
-    var connection = request.accept('echo-protocol', request.origin);
+    var connection = request.accept(config.protocol, request.origin);
     console.log((new Date()) + ' Connection accepted.');
     connection.on('message', function(message) {
         if (message.type === 'utf8') {
