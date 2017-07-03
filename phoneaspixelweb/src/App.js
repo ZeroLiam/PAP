6da2d1b4cd5b2b80201592ffabb39ec0ea2f252b
ZeroLiam <liselotr@gmail.com>
2017-07-03 20:30:37 +0200

    Added Cam container

--- phoneaspixelweb/src/App.js
+++ phoneaspixelweb/src/App.js
@@ -3,6 +3,7 @@ import { Router, Route, browserHistory } from 'react-router';
 
 import './App.css';
 import Index from './containers/Index';
+import Index from './containers/Cam';
 import _ from 'lodash';
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
     return (
       <Router history={browserHistory}>
         <Route path="/" component={Index} />
+          <Route path="/cam" component={Cam} />
       </Router>
     );
   }
---
+++ phoneaspixelweb/src/containers/Cam.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './../App.css';
+
+class Cam extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+        data: {}
+    }
+  }
+
+
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default Cam;
